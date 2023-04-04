02234b32392d00f5b2d3970179f499bbb5b77190
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-04-04 12:11:34 -0400

    added object array pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello World');
 
-let favoriteFood = 'Nachos';
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Mewtwo', height: 2, type: 'Psychic'},
+    { name:'Hitmonlee', height: 1.5, type: 'Fighting'},
+    { name:'Raichu', height: 0.8, type: 'Eletric'}
+];
+
