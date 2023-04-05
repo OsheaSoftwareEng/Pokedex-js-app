a2cd277e15df6f568f3ca4be5915c238d5854751
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-04-05 10:47:09 -0400

    added for loop with condition for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,13 @@ let pokemonList = [
     { name:'Hitmonlee', height: 1.5, type: 'Fighting'},
     { name:'Raichu', height: 0.8, type: 'Eletric'}
 ];
+//starts from 0 index and iterates over the 3 items in the array. Mewtwo being 0, Hitmonlee being 1, Raichu being 2.
+for (let i = 0; i < pokemonList.length; i++){
+    //writes the the list of pokemon into dom from the loop and adds the height next to each pokemon.
+    document.write(pokemonList[i].name  + " height:" + pokemonList[i].height);
+    //condition statement to write a string if the pokemon's height is greater than 1.9.
+    if(pokemonList[i].height > 1.9) {
+        document.write("Wow, that's big!");
+    }   
+}
 
