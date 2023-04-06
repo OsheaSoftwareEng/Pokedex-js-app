2fa2f4ef7bf5d4d4bf0a8d95a9310a9249f34b6e
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-04-06 14:24:35 -0400

    updated pokemon list array to be a function empty array that adds pokemon into it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,38 @@
-
-let pokemonList = [
-    { name: 'Mewtwo', height: 2, type: 'Psychic'},
-    { name:'Hitmonlee', height: 1.5, type: 'Fighting'},
-    { name:'Raichu', height: 0.8, type: 'Eletric'}
-];
-//starts from 0 index and iterates over the 3 items in the array. Mewtwo being 0, Hitmonlee being 1, Raichu being 2.
-for (let i = 0; i < pokemonList.length; i++){
-    //writes the the list of pokemon into dom from the loop and adds the height next to each pokemon.
-    document.write(pokemonList[i].name  + " height:" + pokemonList[i].height);
-    //condition statement to write a string if the pokemon's height is greater than 1.9.
-    if(pokemonList[i].height > 1.9) {
+    //creates a repository function to hold pokemon in the future
+    let pokemonRepository = (function () {
+        //creates a empty array to add pokemon in later.
+        let pokemonList = [];
+        //function to add pokemon into the array the push method adds to an array
+        function add(pokemon) {
+            pokemonList.push(pokemon);
+        }
+        //function to grab all the pokemon in the list with just getAll function
+        function getAll(){
+            return pokemonList;
+        } 
+        return {
+            add: add,
+            getAll: getAll
+        }
+        })();
+        //adding the pokemon into the repository function
+    pokemonRepository.add({name: 'Mewtwo', height: 2, type: 'Psychic'});
+    pokemonRepository.add({name:'Hitmonlee', height: 1.5, type: 'Fighting'});
+    pokemonRepository.add({name:'Raichu', height: 0.8, type: 'Eletric'});
+    //logging all the pokemon in the array into the console
+    console.log(pokemonRepository.getAll());
+    //using pokemonRepository and calling function getAll() calls the array, then adding forEach method works and passing item stays within an IIFE since its within the function.   
+    pokemonRepository.getAll().forEach(function(item){
+        //since pokemonRespository grabbed the array through getAll, it now put the array in the function that holds the parameter item.
+        //item is now each "item" in the array. So you can do item. and retrieve the values from the array.
+        document.write(item.name  + " height:" + item.height);
+    if(item.height > 1.9) {
         document.write("Wow, that's big!");
-    }   
-}
+    }
+});
 
+
+
+    
+    
+  
